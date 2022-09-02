8f3f2ce3f18a7af0162b0f46267df016e711febb
Aleksandr Novikov <novikovav2@gmail.com>
2022-09-02 13:12:29 +1000

    Edit getEvents methods

--- src/api/events/getEvents.ts
+++ src/api/events/getEvents.ts
@@ -5,16 +5,19 @@ import {execute} from "../../db";
 export const getEvents = async (event: YC.CloudFunctionsHttpEvent): Promise<Result> => {
     let result: Result
     const status = event.params.status
+    const userId = event.requestContext.authorizer.user.id
 
-    let query = `SELECT id, authorId, evented_at, isPublic, reason, status, title
-                  FROM events `
+    let query = `SELECT e.id, authorId, evented_at, isPublic, reason, status, title
+                  FROM events e
+                  CROSS JOIN members m
+                  WHERE m.eventId = e.id AND m.userId = '${userId}'`
 
     switch (status) {
         case "active":
-            query = query + ` WHERE status = 'active'`
+            query = query + ` AND e.status = 'active'`
             break
         case "archive":
-            query = query + ` WHERE status = 'archive'`
+            query = query + ` AND e.status = 'archive'`
             break
         default:
             break
