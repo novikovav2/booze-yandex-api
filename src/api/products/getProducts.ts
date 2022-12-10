fdb8fbab5f89e11ee2c6ca27532e39c6118dd6d6
Aleksandr Novikov <novikovav2@gmail.com>
2022-12-10 17:19:39 +1000

    Some changes for using YQL queries with params

--- src/api/products/getProducts.ts
+++ src/api/products/getProducts.ts
@@ -3,13 +3,15 @@ import {YC} from "../../yc";
 import {execute, logger} from "../../db";
 import {SUCCESS} from "../../consts";
 import {Product} from "../../models/product";
+import {TypedValues} from "ydb-sdk";
 
 export const getProducts = async (event: YC.CloudFunctionsHttpEvent): Promise<Result> => {
     logger.info("Start getProducts method")
 
     let result: Result
     const id = event.params.id
-    const query = `SELECT p.id as id,
+    const query = ` DECLARE $eventId AS Utf8;
+                    SELECT p.id as id,
                             p.eventId as eventId,
                             p.title as title,
                             p.price as price,
@@ -21,11 +23,14 @@ export const getProducts = async (event: YC.CloudFunctionsHttpEvent): Promise<Re
                     FROM products p
                     LEFT JOIN users u ON p.buyerId = u.id 
                     LEFT JOIN eaters e ON p.id = e.productId 
-                    WHERE p.eventId = '${id}'
+                    WHERE p.eventId = $eventId
                     group by p.id, p.eventId, p.title, p.price, p.total,
                                 u.id, u.username, u.type
-                    order by title`
-    result = await execute(query)
+                    order by title;`
+    const params = {
+        '$eventId': TypedValues.utf8(id)
+    }
+    result = await execute(query, params)
     logger.info(`Result after select from products: ${JSON.stringify(result)}`)
     if (result.status === SUCCESS) {
         let products: Product[] = []
@@ -43,31 +48,6 @@ export const getProducts = async (event: YC.CloudFunctionsHttpEvent): Promise<Re
                 },
                 eatersCount: item.count.low
             }
-
-            // const queryEater = `SELECT e.id as id,
-            //                            e.productId as productId,
-            //                            e.number as number,
-            //                            e.userId as userId,
-            //                            u.username as username,
-            //                            u.type as type
-            //                 FROM eaters e
-            //                 CROSS JOIN users u
-            //                 WHERE e.userId = u.id
-            //                     AND productId = '${item.id}'`
-            // const eaterResult = await execute(queryEater)
-            // const eaters: Eater[] = []
-            // eaterResult.data.forEach((e) => {
-            //     const eater: Eater = {
-            //         user: {
-            //             id: e.userId,
-            //             username: e.username,
-            //             type: e.type
-            //         },
-            //         count: e.number
-            //     }
-            //     eaters.push(eater)
-            // })
-            // product.eaters = eaters
             products.push(product)
         }
         result = {
