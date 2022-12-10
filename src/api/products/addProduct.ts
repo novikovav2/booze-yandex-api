c4a47813a526dbdce63dbdda0c986e9e30643496
Aleksandr Novikov <novikovav2@gmail.com>
2022-12-10 13:33:14 +1000

    Some changes for using YQL queries with params

--- src/api/products/addProduct.ts
+++ src/api/products/addProduct.ts
@@ -5,6 +5,7 @@ import {NewProduct} from "../../models/product";
 import {v4 as uuid} from "uuid"
 import {SUCCESS} from "../../consts";
 import {clearResult} from "../shared/clearResult";
+import {TypedValues} from "ydb-sdk";
 
 export const addProduct = async (event: YC.CloudFunctionsHttpEvent): Promise<Result> => {
     logger.info("Start addProduct method")
@@ -14,16 +15,40 @@ export const addProduct = async (event: YC.CloudFunctionsHttpEvent): Promise<Res
     const uuidNew = uuid()
 
 
-    const query = `UPSERT INTO products (id, title, eventId, buyerId, total, price )
-                    VALUES ('${uuidNew}', '${product.title}', '${product.eventId}',
-                            '${product.buyerId}', ${product.total}, ${product.price})`
-    result = await execute(query)
+    const query = ` DECLARE $productId AS Utf8;
+                    DECLARE $title AS Utf8;
+                    DECLARE $eventId AS Utf8;
+                    DECLARE $buyerId AS Utf8;
+                    DECLARE $total AS Int32;
+                    DECLARE $price AS Int32;
+                    INSERT INTO products (id, title, eventId, buyerId, total, price )
+                    VALUES ($productId, $title, $eventId,
+                            $buyerId, $total, $price);`
+    const paramsProduct = {
+        '$productId': TypedValues.utf8(uuidNew),
+        '$title': TypedValues.utf8(product.title),
+        '$eventId': TypedValues.utf8(product.eventId),
+        '$buyerId': TypedValues.utf8(product.buyerId),
+        '$total': TypedValues.int32(product.total),
+        '$price': TypedValues.int32(product.price),
+    }
+    result = await execute(query, paramsProduct)
     if (result.status === SUCCESS) {
         for (const eater of product.eaters) {
             const eaterId = uuid()
-            const queryEaters = `UPSERT INTO eaters (id, number, productId, userId)
-                                VALUES ('${eaterId}', 0, '${uuidNew}', '${eater.user.id}')`
-            await execute(queryEaters)
+            const queryEaters = `DECLARE $eaterId AS Utf8;
+                                 DECLARE $number AS Int32;
+                                 DECLARE $productId AS Utf8;
+                                 DECLARE $userId AS Utf8;
+                                 INSERT INTO eaters (id, number, productId, userId)
+                                 VALUES ($eaterId, $number, $productId, $userId);`
+            const paramsEater = {
+                '$eaterId': TypedValues.utf8(eaterId),
+                '$number': TypedValues.int32(0),
+                '$productId': TypedValues.utf8(uuidNew),
+                '$userId': TypedValues.utf8(eater.user.id),
+            }
+            await execute(queryEaters, paramsEater)
         }
         await clearResult(product.eventId)
     }
