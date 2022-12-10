e621c13a2ba6629d28e82ee0db009e97390c2c1b
Aleksandr Novikov <novikovav2@gmail.com>
2022-12-10 12:14:24 +1000

    Some changes for using YQL queries with params

--- src/api/members/joinMember.ts
+++ src/api/members/joinMember.ts
@@ -5,6 +5,7 @@ import {BAD_REQUEST} from "../../consts";
 import {v4 as uuid} from "uuid"
 import {User} from "../../models/user";
 import {clearResult} from "../shared/clearResult";
+import {TypedValues} from "ydb-sdk";
 
 export const joinMember = async (event: YC.CloudFunctionsHttpEvent): Promise<Result> => {
     logger.info("Start joinMember method")
@@ -15,9 +16,17 @@ export const joinMember = async (event: YC.CloudFunctionsHttpEvent): Promise<Res
 
     if (eventId && user.id) {
         const uuidMember = uuid()
-        const query = `INSERT INTO members (id, eventId, userId)
-                   VALUES ('${uuidMember}', '${eventId}', '${user.id}')`
-        result = await execute(query)
+        const query = ` DECLARE $memberId AS Utf8;
+                        DECLARE $eventId AS Utf8;
+                        DECLARE $userId AS Utf8;
+                        INSERT INTO members (id, eventId, userId)
+                        VALUES ($memberId, $eventId, $userId);`
+        const params = {
+            '$memberId': TypedValues.utf8(uuidMember),
+            '$eventId': TypedValues.utf8(eventId),
+            '$userId': TypedValues.utf8(user.id),
+        }
+        result = await execute(query, params)
         await clearResult(eventId)
     } else {
         result = {
