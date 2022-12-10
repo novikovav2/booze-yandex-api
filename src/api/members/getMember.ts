ebac0dbae03b6fff728cc217a966159fe293e0dc
Aleksandr Novikov <novikovav2@gmail.com>
2022-12-10 12:02:53 +1000

    Some changes for using YQL queries with params

--- src/api/members/getMember.ts
+++ src/api/members/getMember.ts
@@ -3,6 +3,7 @@ import {YC} from "../../yc";
 import {execute, logger} from "../../db";
 import {BAD_REQUEST, SUCCESS} from "../../consts";
 import {Member} from "../../models/member";
+import {TypedValues} from "ydb-sdk";
 
 export const getMember = async (event: YC.CloudFunctionsHttpEvent): Promise<Result> => {
     logger.info("Start getMember method")
@@ -10,16 +11,20 @@ export const getMember = async (event: YC.CloudFunctionsHttpEvent): Promise<Resu
     const id = event.params.id
 
     if (id) {
-        const query = `select m.id as id,
+        const query = ` DECLARE $memberId AS Utf8;   
+                        select m.id as id,
                             m.eventId as eventId,
                             u.id as userId,
                             u.username as username,
                             u.type as type
-                    from members m
-                    cross join users u
-                    where m.userId = u.id
-                        and m.id = '${id}'`
-        result = await execute(query)
+                        from members m
+                        cross join users u
+                        where m.userId = u.id
+                            and m.id = $memberId;`
+        const params = {
+            '$memberId': TypedValues.utf8(id)
+        }
+        result = await execute(query, params)
         if (result.status === SUCCESS) {
             logger.info("Data received successfully")
             logger.info(`Data: ${JSON.stringify(result.data)}`)
